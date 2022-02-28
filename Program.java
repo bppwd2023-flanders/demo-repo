8062f511443f08d4290ee5937d9264cc087607e7
Gachagua <EM.Gachagua@baypath.net>
2022-02-28 12:26:17 -0500

    Doing stuff

--- Program.java
+++ Program.java
@@ -3,6 +3,7 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("Jayden Flanders");
         System.out.println("Emma G");
+        System.out.println("emma error");
     }
 
 }
